1e0e83bdef8452b8eac9c115fbbc22a122dea6a1
Victor Ruan Diniz Pereira <victor.diniz@discente.ufma.br>
2022-10-14 13:07:29 -0300

    FEAT: AppWidget

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,19 @@
+
+import 'package:cortana_starting_desktop/src/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SecondScreen(),
+    );
+  }
+}
